9691adbe3cf27916888edbb0577832527450266e
Jarh Lewis <jarhra96@gmail.com>
2020-01-22 23:07:00 -0400

    Initial Commit

---
+++ src/main/java/com/termometrohipico/THSpringBackend/ThSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.termometrohipico.THSpringBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThSpringBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/termometrohipico/THSpringBackend/ThSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.termometrohipico.THSpringBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThSpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
