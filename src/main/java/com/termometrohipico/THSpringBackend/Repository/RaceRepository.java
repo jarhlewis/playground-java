c51d98a08533dfcca084cb1c48f595e32ce1c5ff
Jarh Lewis <jarhra96@gmail.com>
2020-01-22 23:44:00 -0400

    Add repository files

---
+++ src/main/java/com/termometrohipico/THSpringBackend/Repository/RaceRepository.java
@@ -0,0 +1,6 @@
+package com.termometrohipico.THSpringBackend.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RaceRepository extends JpaRepository {
+}
---
+++ src/main/java/com/termometrohipico/THSpringBackend/Repository/TeamMemberRepository.java
@@ -0,0 +1,6 @@
+package com.termometrohipico.THSpringBackend.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamMemberRepository extends JpaRepository {
+}
