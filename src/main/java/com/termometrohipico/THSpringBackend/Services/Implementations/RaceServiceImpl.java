dc655bccaaee11cf257ace5b0ea5cb1a0cd505aa
Jarh Lewis <jarhra96@gmail.com>
2020-01-23 23:58:42 -0400

    Add raceservice interface crud methods. Add boilerplate for service impl

--- src/main/java/com/termometrohipico/THSpringBackend/Services/Implementations/RaceServiceImpl.java
+++ src/main/java/com/termometrohipico/THSpringBackend/Services/Implementations/RaceServiceImpl.java
@@ -1,4 +1,41 @@
 package com.termometrohipico.THSpringBackend.Services.Implementations;
 
-public class RaceServiceImpl {
+import com.termometrohipico.THSpringBackend.Domain.Race;
+import com.termometrohipico.THSpringBackend.Repository.RaceRepository;
+import com.termometrohipico.THSpringBackend.Services.RaceService;
+
+import java.util.List;
+
+public class RaceServiceImpl implements RaceService {
+
+    private RaceRepository raceRepo;
+
+    public RaceServiceImpl (RaceRepository theRaceRepo){
+        this.raceRepo = theRaceRepo;
+    }
+    
+    @Override
+    public List<Race> findAll() {
+        return null;
+    }
+
+    @Override
+    public Race findById() {
+        return null;
+    }
+
+    @Override
+    public void save() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void deleteById() {
+
+    }
 }
--- src/main/java/com/termometrohipico/THSpringBackend/Services/RaceService.java
+++ src/main/java/com/termometrohipico/THSpringBackend/Services/RaceService.java
@@ -1,4 +1,13 @@
 package com.termometrohipico.THSpringBackend.Services;
 
+import com.termometrohipico.THSpringBackend.Domain.Race;
+
+import java.util.List;
+
 public interface RaceService {
+    public List<Race> findAll();
+    public Race findById();
+    public void save();
+    public void update();
+    public void deleteById();
 }
